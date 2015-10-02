03b4146db2b19c9393be816faef98029cda940e5
gabrielalberione <gabrielalberione@gmail.com>
2015-10-02 20:55:51 -0300

    v1.0

---
+++ platforms/android/assets/www/js/libs/reproductor_audio.js
@@ -0,0 +1,95 @@
+// Audio player
+//
+var my_media = null;
+var mediaTimer = null;
+var duracion_sonido = 0;
+var position_sonido = 0;
+var arrancar_de_nuevo = true;
+
+// Play audio
+//
+function playAudio() {
+	// Create Media object from src
+	if(arrancar_de_nuevo == true){
+		my_media = new Media(audio_src, onSuccess, onError);
+		arrancar_de_nuevo = false;
+	}
+	$("#repro_play").hide();
+	$("#repro_pause").show();
+	// Play audio
+	my_media.play();
+
+	// Update my_media position every second
+	if (mediaTimer == null) {
+		mediaTimer = setInterval(function() {
+			// get my_media position
+			my_media.getCurrentPosition(
+				// success callback
+				function(position) {
+					if (position > -1) {
+						position_sonido = position;
+						duracion_sonido = my_media.getDuration();
+						setAudioPosition(position, my_media.getDuration());
+					}
+				},
+				// error callback
+				function(e) {
+					console.log("Error getting pos=" + e);
+					setAudioPosition("Error: " + e);
+				}
+			);
+		}, 1000);
+	}
+}
+
+// Pause audio
+//
+function pauseAudio() {
+	$("#repro_play").show();
+	$("#repro_pause").hide();
+	if (my_media) {
+		my_media.pause();
+	}
+}
+
+// Stop audio
+//
+function stopAudio() {
+	if (my_media) {
+		my_media.stop();
+	}
+	clearInterval(mediaTimer);
+	mediaTimer = null;
+	arrancar_de_nuevo = true;
+	$("#repro_play").show();
+	$("#repro_pause").hide();
+	$("#reproduccion_barra").width( "0%" );
+}
+
+// onSuccess Callback
+//
+function onSuccess() {
+	console.log("playAudio():Audio Success");
+}
+
+// onError Callback
+//
+function onError(error) {
+	alert('code: '    + error.code    + '\n' +
+		  'message: ' + error.message + '\n');
+}
+
+// Set audio position
+//
+function setAudioPosition(position, duracion) {
+	var porc = position * 100 / duracion;
+  //  document.getElementById('audio_position').innerHTML = ;
+	$("#reproduccion_barra").width( Math.ceil(porc) + "%" );
+}
+
+
+function adelantar_audio(e){	
+	var perc = e.offsetX/ $(this).width() * 100;
+	seconds = perc * duracion_sonido / 100;
+	my_media.seekTo(seconds*1000);	
+}
---
+++ www/js/utiles.js
@@ -0,0 +1,295 @@
+function encrypt(string, key) {
+	var result = '';
+	/* recorre cada caracter del string */
+	for (var i = 0, len = string.length; i < len; i++) {					
+		char = substr(string, i, 1); // busca un caracter
+		//char = string[i];
+		mod = (i % key.length); // trae el modulo de la division
+		keychar = substr(key, (mod), 1);
+		//echo $char.'|'.ord($char).'|'.$keychar.'|'.ord($keychar).'|'.(ord($char)+ord($keychar)).'|'.(chr(ord($char)+ord($keychar))).'<br>';
+		char = chr(ord(char)+ord(keychar)); // ord: devuelve el primer ascii del string
+		result += char;
+	}
+	res = base64_encode(result); // luego le hace base64 para codificarlo (estaria de mas)
+	//echo $res;
+	return res;
+}
+	
+function decrypt(string, key) {
+   result = '';
+   string = base64_decode(string);
+   for (var i = 0, len = string.length; i < len; i++) {	
+	  char = substr(string, i, 1);
+	  mod = (i % key.length); // trae el modulo de la division
+	  keychar = substr(key, (mod), 1);
+	  char = chr(ord(char)-ord(keychar));
+	  result += char;
+   }
+   return result;
+}
+
+function base64_decode(data) {
+  //  discuss at: http://phpjs.org/functions/base64_decode/
+  // original by: Tyler Akins (http://rumkin.com)
+  // improved by: Thunder.m
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //    input by: Aman Gupta
+  //    input by: Brett Zamir (http://brett-zamir.me)
+  // bugfixed by: Onno Marsman
+  // bugfixed by: Pellentesque Malesuada
+  // bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //   example 1: base64_decode('S2V2aW4gdmFuIFpvbm5ldmVsZA==');
+  //   returns 1: 'Kevin van Zonneveld'
+  //   example 2: base64_decode('YQ===');
+  //   returns 2: 'a'
+
+  var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+  var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+    ac = 0,
+    dec = '',
+    tmp_arr = [];
+
+  if (!data) {
+    return data;
+  }
+
+  data += '';
+
+  do { // unpack four hexets into three octets using index points in b64
+    h1 = b64.indexOf(data.charAt(i++));
+    h2 = b64.indexOf(data.charAt(i++));
+    h3 = b64.indexOf(data.charAt(i++));
+    h4 = b64.indexOf(data.charAt(i++));
+
+    bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+
+    o1 = bits >> 16 & 0xff;
+    o2 = bits >> 8 & 0xff;
+    o3 = bits & 0xff;
+
+    if (h3 == 64) {
+      tmp_arr[ac++] = String.fromCharCode(o1);
+    } else if (h4 == 64) {
+      tmp_arr[ac++] = String.fromCharCode(o1, o2);
+    } else {
+      tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
+    }
+  } while (i < data.length);
+
+  dec = tmp_arr.join('');
+
+  return dec.replace(/\0+$/, '');
+}
+
+function base64_encode(data) {
+  //  discuss at: http://phpjs.org/functions/base64_encode/
+  // original by: Tyler Akins (http://rumkin.com)
+  // improved by: Bayron Guevara
+  // improved by: Thunder.m
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Rafał Kukawski (http://kukawski.pl)
+  // bugfixed by: Pellentesque Malesuada
+  //   example 1: base64_encode('Kevin van Zonneveld');
+  //   returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
+  //   example 2: base64_encode('a');
+  //   returns 2: 'YQ=='
+
+  var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+  var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+    ac = 0,
+    enc = '',
+    tmp_arr = [];
+
+  if (!data) {
+    return data;
+  }
+
+  do { // pack three octets into four hexets
+    o1 = data.charCodeAt(i++);
+    o2 = data.charCodeAt(i++);
+    o3 = data.charCodeAt(i++);
+
+    bits = o1 << 16 | o2 << 8 | o3;
+
+    h1 = bits >> 18 & 0x3f;
+    h2 = bits >> 12 & 0x3f;
+    h3 = bits >> 6 & 0x3f;
+    h4 = bits & 0x3f;
+
+    // use hexets to index into b64, and append result to encoded string
+    tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+  } while (i < data.length);
+
+  enc = tmp_arr.join('');
+
+  var r = data.length % 3;
+
+  return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
+}
+
+function chr(codePt) {
+  //  discuss at: http://phpjs.org/functions/chr/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Brett Zamir (http://brett-zamir.me)
+  //   example 1: chr(75) === 'K';
+  //   example 1: chr(65536) === '\uD800\uDC00';
+  //   returns 1: true
+  //   returns 1: true
+
+  if (codePt > 0xFFFF) { // Create a four-byte string (length 2) since this code point is high
+    //   enough for the UTF-16 encoding (JavaScript internal use), to
+    //   require representation with two surrogates (reserved non-characters
+    //   used for building other characters; the first is "high" and the next "low")
+    codePt -= 0x10000;
+    return String.fromCharCode(0xD800 + (codePt >> 10), 0xDC00 + (codePt & 0x3FF));
+  }
+  return String.fromCharCode(codePt);
+}
+
+function ord(string) {
+  //  discuss at: http://phpjs.org/functions/ord/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // bugfixed by: Onno Marsman
+  // improved by: Brett Zamir (http://brett-zamir.me)
+  //    input by: incidence
+  //   example 1: ord('K');
+  //   returns 1: 75
+  //   example 2: ord('\uD800\uDC00'); // surrogate pair to create a single Unicode character
+  //   returns 2: 65536
+
+  var str = string + '',
+    code = str.charCodeAt(0);
+  if (0xD800 <= code && code <= 0xDBFF) { // High surrogate (could change last hex to 0xDB7F to treat high private surrogates as single characters)
+    var hi = code;
+    if (str.length === 1) {
+      return code; // This is just a high surrogate with no following low surrogate, so we return its value;
+      // we could also throw an error as it is not a complete character, but someone may want to know
+    }
+    var low = str.charCodeAt(1);
+    return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;
+  }
+  if (0xDC00 <= code && code <= 0xDFFF) { // Low surrogate
+    return code; // This is just a low surrogate with no preceding high surrogate, so we return its value;
+    // we could also throw an error as it is not a complete character, but someone may want to know
+  }
+  return code;
+}
+
+function substr(str, start, len) {
+  //  discuss at: http://phpjs.org/functions/substr/
+  //     version: 909.322
+  // original by: Martijn Wieringa
+  // bugfixed by: T.Wild
+  // improved by: Onno Marsman
+  // improved by: Brett Zamir (http://brett-zamir.me)
+  //  revised by: Theriault
+  //        note: Handles rare Unicode characters if 'unicode.semantics' ini (PHP6) is set to 'on'
+  //   example 1: substr('abcdef', 0, -1);
+  //   returns 1: 'abcde'
+  //   example 2: substr(2, 0, -6);
+  //   returns 2: false
+  //   example 3: ini_set('unicode.semantics',  'on');
+  //   example 3: substr('a\uD801\uDC00', 0, -1);
+  //   returns 3: 'a'
+  //   example 4: ini_set('unicode.semantics',  'on');
+  //   example 4: substr('a\uD801\uDC00', 0, 2);
+  //   returns 4: 'a\uD801\uDC00'
+  //   example 5: ini_set('unicode.semantics',  'on');
+  //   example 5: substr('a\uD801\uDC00', -1, 1);
+  //   returns 5: '\uD801\uDC00'
+  //   example 6: ini_set('unicode.semantics',  'on');
+  //   example 6: substr('a\uD801\uDC00z\uD801\uDC00', -3, 2);
+  //   returns 6: '\uD801\uDC00z'
+  //   example 7: ini_set('unicode.semantics',  'on');
+  //   example 7: substr('a\uD801\uDC00z\uD801\uDC00', -3, -1)
+  //   returns 7: '\uD801\uDC00z'
+
+  var i = 0,
+    allBMP = true,
+    es = 0,
+    el = 0,
+    se = 0,
+    ret = '';
+  str += '';
+  var end = str.length;
+
+  // BEGIN REDUNDANT
+  this.php_js = this.php_js || {};
+  this.php_js.ini = this.php_js.ini || {};
+  // END REDUNDANT
+  switch ((this.php_js.ini['unicode.semantics'] && this.php_js.ini['unicode.semantics'].local_value.toLowerCase())) {
+    case 'on':
+      // Full-blown Unicode including non-Basic-Multilingual-Plane characters
+      // strlen()
+      for (i = 0; i < str.length; i++) {
+        if (/[\uD800-\uDBFF]/.test(str.charAt(i)) && /[\uDC00-\uDFFF]/.test(str.charAt(i + 1))) {
+          allBMP = false;
+          break;
+        }
+      }
+
+      if (!allBMP) {
+        if (start < 0) {
+          for (i = end - 1, es = (start += end); i >= es; i--) {
+            if (/[\uDC00-\uDFFF]/.test(str.charAt(i)) && /[\uD800-\uDBFF]/.test(str.charAt(i - 1))) {
+              start--;
+              es--;
+            }
+          }
+        } else {
+          var surrogatePairs = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
+          while ((surrogatePairs.exec(str)) != null) {
+            var li = surrogatePairs.lastIndex;
+            if (li - 2 < start) {
+              start++;
+            } else {
+              break;
+            }
+          }
+        }
+
+        if (start >= end || start < 0) {
+          return false;
+        }
+        if (len < 0) {
+          for (i = end - 1, el = (end += len); i >= el; i--) {
+            if (/[\uDC00-\uDFFF]/.test(str.charAt(i)) && /[\uD800-\uDBFF]/.test(str.charAt(i - 1))) {
+              end--;
+              el--;
+            }
+          }
+          if (start > end) {
+            return false;
+          }
+          return str.slice(start, end);
+        } else {
+          se = start + len;
+          for (i = start; i < se; i++) {
+            ret += str.charAt(i);
+            if (/[\uD800-\uDBFF]/.test(str.charAt(i)) && /[\uDC00-\uDFFF]/.test(str.charAt(i + 1))) {
+              se++; // Go one further, since one of the "characters" is part of a surrogate pair
+            }
+          }
+          return ret;
+        }
+        break;
+      }
+      // Fall-through
+    case 'off':
+      // assumes there are no non-BMP characters;
+      //    if there may be such characters, then it is best to turn it on (critical in true XHTML/XML)
+    default:
+      if (start < 0) {
+        start += end;
+      }
+      end = typeof len === 'undefined' ? end : (len < 0 ? len + end : len + start);
+      // PHP returns false if start does not fall within the string.
+      // PHP returns false if the calculated end comes before the calculated start.
+      // PHP returns an empty string if start and end are the same.
+      // Otherwise, PHP returns the portion of the string from start to end.
+      return start >= str.length || start < 0 || start > end ? !1 : str.slice(start, end);
+  }
+  return undefined; // Please Netbeans
+}
